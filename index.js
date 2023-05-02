509357dfc2bb810c0ba40b9d9c0e3f674ff6ead9
mohammadashrafulislam <ixdell41@gmail.com>
2023-05-02 18:24:50 +0600

    recipes data added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const chef = require('./data/chef.json');
+const recipes = require('./data/recipes.json')
 const cors = require('cors');
 const app = express();
 const port = 3000;
@@ -10,6 +11,9 @@ app.get('/', (req, res) => {
 app.use(cors())
 app.get('/chef', (req, res) =>{
     res.send(chef)
+})
+app.get('/recipes', (req, res) =>{
+    res.send(recipes)
 })
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
