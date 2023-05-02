7d569e6f92e675cb64d926d4b680de785cb64e99
mohammadashrafulislam <ixdell41@gmail.com>
2023-05-02 16:25:40 +0600

    express js cors added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const chef = require('./data/chef.json');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Running server')
 })
+app.use(cors())
 app.get('/chef', (req, res) =>{
     res.send(chef)
 })
