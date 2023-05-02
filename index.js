3c8e0f863cc955b341229af500182e3e3999634b
mohammadashrafulislam <ixdell41@gmail.com>
2023-05-02 16:13:55 +0600

    Added chef data to index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const chef = require('./data/chef.json');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Running server')
 })
-
+app.get('/chef', (req, res) =>{
+    res.send(chef)
+})
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
 })
